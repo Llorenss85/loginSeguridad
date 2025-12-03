e27f254e5405e02bdb8e36c0cc7657c81547a72e
Elias Llorens <eliasllorens85@gmail.com>
2025-12-03 10:54:23 -0300

    v0.1.1

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request,render_template
 import mariadb
 
 app = Flask(__name__)
 
-conn = mariadb.connect(
+conn = mariadb.connect( #Uitiliza el i import de mariadb
     user="",
     password="",
-    host="",
     port=3306,
-    database="passwordProy"
+    database=""
 )
 
-cursor = conn.cursor()
-
+cursor = conn.cursor() # generó el cursor que permite la interaccion con la base de datos.
+# app.route -> importante para poder visitar estas paginas al cargarla con el local host de python
 @app.route("/loginv1", methods=["POST"])
 def login():
     password = request.form["password"]
@@ -20,6 +19,9 @@ def login():
     cursor.execute("INSERT INTO usario (password) VALUES (?)", (password,))
     conn.commit()
 
-    return open("index.html").read() 
-app.run(port=5000)
+    return render_template("index.html") #se encarga de renderizar el index.html, no necesita su ubicacion, por que  flask gracias a su estructura con los directorios, sabe buscarlos.
+@app.route("/")
+def index():
+    return render_template("index.html")
+app.run(port=5000) #importante para que ande el local host
 
---
+++ static/script.js
@@ -0,0 +1,61 @@
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const inputPasword= document.getElementById("inputPasword");
+    const botonCandado= document.getElementById("img-Pasword");
+    const inputSubmit = document.getElementById("submit-contraseña");
+    const content=[]
+
+    botonCandado.addEventListener("click", function() {
+        //img, se obtiene el atributo porque si se usa solo el "src" se pasas un enlace https. ()
+        const img2="/static/img/password-svgrepo-com.svg"
+        const img= "/static/img/unk-password-svgrepo-com.svg"
+        const imgActual=botonCandado.getAttribute("src");
+
+        if(imgActual ==img){ // colocar imagen bloqueada
+                  
+            botonCandado.src=img2
+            inputPasword.type="password"
+            
+        }
+        else{// colocar imagen desbloqueada
+            botonCandado.src=img
+            inputPasword.type="text"
+            
+        }
+    
+    });
+
+    inputPasword.addEventListener("input",(elem)=>{
+        var letra=elem.data
+        var label=document.getElementById("labelPasword")
+        const advertenciaPass= document.getElementById("label-verification"); 
+        //accion de verificacion
+        const verification=document.getElementById("label-verification")
+        //accion de decicion de verificacion
+        eliminarOAgregarLetra(content,letra)
+        agregarAdvertencias(content,advertenciaPass)
+
+    })
+
+
+    function eliminarOAgregarLetra(lista,letra){
+        if(letra == null) lista.pop(); else lista.push(letra)
+    }
+
+
+    function agregarAdvertencias(lista,elemento){
+        if(lista.length>=12 && lista.length<=14){
+            elemento.textContent ="Contraseña segura !"
+        }
+        else{
+            elemento.textContent ="contraseña debil.."
+            
+        }
+    }
+
+    inputSubmit.addEventListener("click", ()=>{
+        
+    })
+});
+
